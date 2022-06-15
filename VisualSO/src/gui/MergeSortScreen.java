32a5120e340a79ef7ffa30730593b6e5b1d689c1
katakd <89955319+katakd@users.noreply.github.com>
2022-06-15 22:52:44 +0700

    Sample Swing-Base Visualizer and data requirement of sorting package

--- VisualSO/src/gui/MergeSortScreen.java
+++ VisualSO/src/gui/MergeSortScreen.java
@@ -3,37 +3,52 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Arrays;
 import java.util.Collections;
-
 import javax.swing.BorderFactory;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
-public class MergeSortFrame extends Demonstration{
+public class MergeSortScreen extends SortingScreen{
+	@Override
+	public String[] infoArrayStep() {
+		return infoArrayStep;
+	}
+	@Override
+	public int[][] mainArrayStep(){
+		return mainArrayStep;
+	}
+	@Override
+	public int[][] subArrayStep(){
+		return subArrayStep;
+	}
+	
+	int x = 0;
+	int y = 0;
+	int width;
+	int height;
+	int padding = 5;
 	@Override
 	public JPanel main(int[] array) {
 		JPanel main =  new JPanel() {
-			int x = 0;
-			int y = 0;
-			int width;
-			int height;
-			int padding = 5;
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
 				width =(int) getWidth()/array.length;
-				height=(int) getHeight()/max(array);
+				if (max(array) !=0) {
+				height=(int) getHeight()/max(mainArray);}
+				else {height = 0;}
 				x = (getWidth()- min(width,60+padding)*array.length)/2;
+				int index = 0;
 				for (int i:array) {
 					g.setColor(myPINK);
 				    g.fillRect(x, y-i*height+ getHeight(),min(width-padding,60),i*height);
 				    x += min(width,60+padding) ;
-				    
 				}
 				x = 0;
 			}
+			
 		};
-		//main.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		return main;
 	}
 	
@@ -42,21 +57,31 @@ public class MergeSortFrame extends Demonstration{
 		JPanel sub =  new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
-			
+				super.paintComponent(g);
+				width =(int) getWidth()/array.length;
+				if (max(array) !=0) {
+				height=(int) getHeight()/max(mainArray);}
+				else {height = 0;}
+				x = (getWidth()- min(width,60+padding)*array.length)/2;
+				int index = 0;
+				for (int i:array) {
+					g.setColor(myGREEN);
+				    g.fillRect(x, y-i*height+ getHeight(),min(width-padding,60),i*height);
+				    x += min(width,60+padding) ;
+				}
+				x = 0;
 		    }
 	     };
-	    //sub.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    return sub;
 	}
 	@Override
-	public JPanel animation(int[] array) {
+	public JPanel animation(int[] position1, int[]position2) {
 		JPanel animation =  new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
-			
+					
 		    }
 	     };
-	    //animation.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    return animation;
 	}
 }
--- VisualSO/src/gui/RadixSortScreen.java
+++ VisualSO/src/gui/RadixSortScreen.java
@@ -7,7 +7,24 @@ import javax.swing.BorderFactory;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
-public class RadixSortFrame extends Demonstration {
+public class RadixSortScreen extends SortingScreen {
+	int x = 0;
+	int y = 0;
+	int width;
+	int height;
+	int padding = 5;
+	@Override
+	public String[] infoArrayStep() {
+		return infoArrayStep2;
+	}
+	@Override
+	public int[][] mainArrayStep(){
+		return mainArrayStep2;
+	}
+	@Override
+	public int[][] subArrayStep(){
+		return subArrayStep2;
+	}
 	@Override
 	public JPanel main(int[] array) {
 		JPanel main =  new JPanel() {
@@ -20,7 +37,9 @@ public class RadixSortFrame extends Demonstration {
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
 				width =(int) getWidth()/array.length;
-				height=(int) getHeight()/max(array);
+				if (max(array) !=0) {
+				height=(int) getHeight()/max(mainArray);}
+				else {height = 0;}
 				x = (getWidth()- min(width,60+padding)*array.length)/2;
 				for (int i:array) {
 					g.setColor(myYELLOW);
@@ -31,30 +50,42 @@ public class RadixSortFrame extends Demonstration {
 				x = 0;
 			}
 		};
-		//main.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		return main;
 	}
 	
 	@Override
 	public JPanel sub(int[] array) {
 		JPanel sub =  new JPanel() {
+			int x =30;
+			int counter = 0;
 			@Override
 			public void paintComponent(Graphics g) {
-			
+				super.paintComponent(g);
+				if (max(array) !=0) {
+				height=(int) getHeight()/max(mainArray);}
+				else {height = 0;}
+				for (int i: array) {
+					g.setColor(Color.GRAY);
+				    g.fillRect(x, y-i*height+ getHeight()-30,60,i*height);
+				    g.setColor(Color.BLACK);
+				    g.drawLine(x,getHeight()-30 , x+60,getHeight()-30);
+				    g.drawString(Integer.toString(counter), x+30, getHeight()-10);
+				    x += 60+padding ;
+				    counter++;
+				}
+				x = 30;
 		    }
 	     };
-	    //sub.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    return sub;
 	}
 	@Override
-	public JPanel animation(int[] array) {
+	public JPanel animation(int[] position1, int[] position2) {
 		JPanel animation =  new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
 			
 		    }
 	     };
-	    //animation.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	    return animation;
 	}
 }
