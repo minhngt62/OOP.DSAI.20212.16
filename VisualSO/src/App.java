eaf9aeb2ba4280781d8a2686048c2f01bb4fa976
katakd <89955319+katakd@users.noreply.github.com>
2022-06-23 23:57:12 +0700

    (Swing-based) create HomeScreen and its HomeController, update animation for Radix Sort( still unfinished), in-progress refactor SortingScreen and separate to its SortingController( still unfinished),update App follow HomeScreen

--- VisualSO/src/App.java
+++ VisualSO/src/App.java
@@ -1,19 +1,13 @@
-import gui.CountingSortFrame;
-import gui.MergeSortFrame;
-import gui.RadixSortFrame;
-import gui.Screen;
+import view.CountingSortScreen;
+import view.HomeScreen;
+import view.MergeSortScreen;
+import view.RadixSortScreen;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MergeSortFrame ms = new MergeSortFrame();
-		ms.setVisible(false);
-		CountingSortFrame cs = new CountingSortFrame();
-		cs.setVisible(false);
-		RadixSortFrame rs = new RadixSortFrame();
-		rs.setVisible(true);
-
+		new HomeScreen().setVisible(true);
 	}
 
 }
