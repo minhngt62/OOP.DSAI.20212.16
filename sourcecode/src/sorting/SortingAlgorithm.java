8bba8b81ea54205aa80d0434fcbd9e49fa20286a
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-06-25 17:24:08 +0700

    Create abstract class for sorting algorithms
    - SortingAlgorithm.java
    - ArrayUtils.java: Add some utilities to process pure arrays easily.

---
+++ sourcecode/src/sorting/SortingAlgorithm.java
@@ -0,0 +1,40 @@
+package sorting;
+
+import util.ArrayUtils;
+
+public abstract class SortingAlgorithm {
+	int[][] arrayLog;
+	int[][] tempLog;
+	int[][] pointerLog;
+	String[] guideLog;
+	
+	int numSteps = 0;
+	int[] inputArray;
+	
+	public SortingAlgorithm(int[] inputArray) {
+		this.inputArray = inputArray;
+		
+		arrayLog = new int[1][inputArray.length];
+		tempLog = new int[1][inputArray.length];
+		pointerLog = new int[1][2]; // two pointers per step
+		guideLog = new String[1];
+	}
+	
+	public abstract void sort();
+	
+	void addLogs(int[] array, int[] temp, int[] pointer, String guide) {
+		if (arrayLog.length == numSteps) {
+			arrayLog = ArrayUtils.resizeIntegers(arrayLog);
+			tempLog = ArrayUtils.resizeIntegers(tempLog);
+			pointerLog = ArrayUtils.resizeIntegers(pointerLog);
+			guideLog = ArrayUtils.resizeStrings(guideLog);
+		}
+		
+		arrayLog[numSteps] = array.clone();
+		tempLog[numSteps] = temp.clone();
+		pointerLog[numSteps] = pointer;
+		guideLog[numSteps] = guide;
+		
+		numSteps++;
+	}
+}
---
+++ sourcecode/src/util/ArrayUtils.java
@@ -0,0 +1,30 @@
+package util;
+
+public class ArrayUtils {
+	public static int max(int[] array) {
+		int max = array[0];
+		for (int element : array) {
+			if (element > max) {
+				max = element;
+			}
+		}
+		return max;
+	}
+	
+	public static int[][] resizeIntegers(int[][] array) {
+		int[][] resizedArray = new int[array.length * 2][array[0].length];
+		for (int i = 0; i < array.length; i++) {
+			resizedArray[i] = array[i];
+		}
+		return resizedArray;
+	}
+	
+	public static String[] resizeStrings(String[] array) {
+		String[] resizedArray = new String[array.length * 2];
+		for (int i = 0; i < array.length; i++) {
+			resizedArray[i] = array[i];
+		}
+		return resizedArray;
+	}
+
+}
