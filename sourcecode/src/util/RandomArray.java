69d84b9f73381a5264c9d7afbc4883ceeecf2e86
namnguyen16 <103294654+namnguyen16@users.noreply.github.com>
2022-06-30 13:44:55 +0700

    Create data

---
+++ sourcecode/src/util/RandomArray.java
@@ -0,0 +1,16 @@
+
+import java.util.Random;
+
+public class RandomArray {
+    public static int[] random_array(int n){
+        int[] result = new int[n];
+        for (int i = 0; i < result.length; i++){
+            Random generator = new Random();
+            int k = generator.nextInt(2*n) + 1;
+            if (DataProcessing.notIncludes(result,k)){
+                result[i] = k;
+            }
+        }
+        return result ;
+    }
+}
