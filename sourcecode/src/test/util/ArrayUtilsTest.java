ac1969d47eddd88ef8330cb4709ccc3b43643704
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2022-07-14 18:26:58 +0700

    Upload the test for view and controller

--- sourcecode/src/test/util/ArrayUtilsTest.java
+++ sourcecode/src/test/util/ArrayUtilsTest.java
@@ -1,4 +1,4 @@
-package test.utils;
+package test.util;
 
 import visualso.util.ArrayUtils;
 
---
+++ sourcecode/src/test/view/CountingSortScreenTest.java
@@ -0,0 +1,10 @@
+package test.view;
+
+import visualso.view.CountingSortScreen;
+
+public class CountingSortScreenTest {
+	
+	public static void main(String[] args) {
+		new CountingSortScreen(); // If it cannot run, it is the faulty in modified mvc pattern ops
+	}
+}
